4bfa1a17beeaf66fd58218975bd6cc26cee5e75d
Michael Reyes <maik1234reyes@gmail.com>
2026-07-07 15:50:07 -0500

    Decode Message

--- WebHook/pkg/github/services/decode_message.go
+++ WebHook/pkg/github/services/decode_message.go
@@ -1,14 +1,30 @@
 package services
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"triggo/pkg/github/model/push"
+)
 
-func (s *Services) DecodeMessage(event string, body string) {
+func (s *Services) DecodeMessage(event string, body []byte) {
 
 	switch event {
 	case "branch":
 		fmt.Println("In this case the event it´s a branch")
 	case "push":
 		fmt.Println("In this case the event it´s a a push")
+
+		var push push.GithubPush
+
+		err := json.Unmarshal(body, &push)
+
+		if err != nil {
+			fmt.Println("Error to decode message")
+			return
+		}
+
+		fmt.Println(push)
+
 	default:
 		fmt.Printf("In this case the event is: %s \n", event)
 	}
