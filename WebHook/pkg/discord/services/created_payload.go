af0800e3459ca4be4c578c17899e53d729d43092
Michael Reyes <maik1234reyes@gmail.com>
2026-07-07 17:28:41 -0500

    Discord implementation

--- WebHook/pkg/discord/services/created_payload.go
+++ WebHook/pkg/discord/services/created_payload.go
@@ -9,7 +9,7 @@ func (s *Services) CreateDiscordPayload(e embed.Embed) payload.Payload {
 
 	payload := payload.Payload{
 		Username:  "Informante Moik",
-		AvatarUrl: "https://ysqz0oydi7thsqmt.public.blob.vercel-storage.com/PHOTO-2025-12-01-23-25-50.jpg",
+		AvatarUrl: "https://ysqz0oydi7thsqmt.public.blob.vercel-storage.com/Moik.jpg",
 	}
 
 	payload.Embeds = []embed.Embed{e}
