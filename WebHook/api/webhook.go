eb55a01ef0b87a1cdeab47b5f2c0bf1ac248a0db
Michael Reyes <maik1234reyes@gmail.com>
2026-07-07 15:05:44 -0500

    remove godotenv

--- WebHook/api/webhook.go
+++ WebHook/api/webhook.go
@@ -1,21 +1,13 @@
 package api
 
 import (
-	"log"
 	"net/http"
 	"triggo/pkg/config"
 	"triggo/pkg/github/handler"
 	"triggo/pkg/github/services"
-
-	"github.com/joho/godotenv"
 )
 
 func Webhook(w http.ResponseWriter, r *http.Request) {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
 
 	config := config.NewConfig()
 
--- WebHook/go.mod
+++ WebHook/go.mod
@@ -1,5 +1,3 @@
 module triggo
 
 go 1.26.4
-
-require github.com/joho/godotenv v1.5.1 // indirect
