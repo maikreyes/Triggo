02c426fee38b891471e3f3c5ac6acd0bbd4c2983
Michael Reyes <maik1234reyes@gmail.com>
2026-07-07 15:01:08 -0500

    using godotenv

--- WebHook/api/webhook.go
+++ WebHook/api/webhook.go
@@ -1,13 +1,21 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"triggo/pkg/config"
 	"triggo/pkg/github/handler"
 	"triggo/pkg/github/services"
+
+	"github.com/joho/godotenv"
 )
 
 func Webhook(w http.ResponseWriter, r *http.Request) {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	config := config.NewConfig()
 
