05abbe15f6c5180562fdfd4bdf805695d14df271
Michael Reyes <maik1234reyes@gmail.com>
2026-07-10 12:46:16 -0500

    fix:remove innecesari comments

--- WebHook/cmd/main.go
+++ WebHook/cmd/main.go
@@ -5,20 +5,16 @@ import (
 	"net/http"
 	"os"
 
-	// Importas tu paquete api (ajusta "triggo/api" según el nombre real de tu módulo)
 	"triggo/api"
 )
 
 func main() {
 
-	// 2. Crear un enrutador local
 	mux := http.NewServeMux()
 
-	// 3. Conectar tus funciones de Vercel a las rutas locales
 	mux.HandleFunc("/api/setup", api.SetupHandler)
 	mux.HandleFunc("/api/webhook", api.Webhook)
 
-	// 4. Iniciar el servidor
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -26,6 +22,5 @@ func main() {
 
 	log.Printf("🚀 Servidor local corriendo en http://localhost:%s\n", port)
 
-	// La magia de Go: esto mantiene el servidor vivo escuchando peticiones
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
